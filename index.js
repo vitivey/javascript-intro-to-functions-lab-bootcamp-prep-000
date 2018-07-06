ca64d55b8362bbd92372e81e9343664884584daf
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-06 15:39:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log (string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) {if (string===string.toLowerCase()) {return "I can't hear you"} 
-elseif (string===string.toUpperCase()) {return "YES INDEED!"} 
-elseif (string==="I love you, Grandma.") {return "I love you, too"} 
+else if (string===string.toUpperCase()) {return "YES INDEED!"} 
+else if (string==="I love you, Grandma.") {return "I love you, too"} 
 else {return string}}
